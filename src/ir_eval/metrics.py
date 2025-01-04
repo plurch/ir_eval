9078a5b391fd1b8672b844d14cad7ccb823c1e06
Patrick Lurch <plurch@users.noreply.github.com>
2025-01-04 13:39:33 -0500

    add precision function

--- src/ir_eval/metrics.py
+++ src/ir_eval/metrics.py
@@ -11,7 +11,7 @@ def recall(actual: npt.NDArray, predicted: npt.NDArray, k: int) -> float:
   Recall is defined as the ratio of the total number of relevant documents retrieved
   within the top-k predictions to the total number of relevant documents in the database.
 
-  Recall  =  Total number of documents retrieved that are relevant/Total number of relevant documents in the database.
+  Recall =  Total number of documents retrieved that are relevant/Total number of relevant documents in the database.
 
   Parameters:
     actual (npt.NDArray): An array of ground truth relevant items.
@@ -33,6 +33,41 @@ def recall(actual: npt.NDArray, predicted: npt.NDArray, k: int) -> float:
     - This function assumes the `predicted` array is sorted in descending order of relevance.
     - If k is larger than the length of the `predicted` array, it will consider the entire array.
   """
-  act_set = set(actual)
-  pred_set = set(predicted[:k])
-  return len(act_set & pred_set) / float(len(act_set))
+  actual_set = set(actual)
+  top_k_predictions = set(predicted[:k])
+  count_relevant_in_top_k = len(actual_set.intersection(top_k_predictions))
+  return count_relevant_in_top_k / float(len(actual_set))
+
+def precision(actual: npt.NDArray, predicted: npt.NDArray, k: int) -> float:
+  """
+  Calculate the precision@k metric.
+
+  Precision is defined as the ratio of the total number of relevant documents retrieved
+  within the top-k predictions to the total number of returned documents (k).
+
+  Precision =  Total number of documents retrieved that are relevant/Total number of documents that are retrieved.
+
+  Parameters:
+    actual (npt.NDArray): An array of ground truth relevant items.
+    predicted (npt.NDArray): An array of predicted items, ordered by relevance.
+    k (int): The number of top predictions to consider.
+
+  Returns:
+    float: The precision value at rank k, ranging from 0 to 1.
+           A value of 1 indicates perfect precision, while 0 indicates no relevant documents retrieved.
+
+  Example:
+    >>> actual = np.array([1, 2, 3, 4])
+    >>> predicted = np.array([4, 2, 6, 1, 7])
+    >>> k = 3
+    >>> precision(actual, predicted, k)
+    0.5  # (2 relevant documents retrieved out of 4)
+
+  Notes:
+    - This function assumes the `predicted` array is sorted in descending order of relevance.
+    - If k is larger than the length of the `predicted` array, it will consider the entire array.
+  """
+  actual_set = set(actual)
+  top_k_predictions = set(predicted[:k])
+  count_relevant_in_top_k = len(actual_set.intersection(top_k_predictions))
+  return count_relevant_in_top_k / float(k)
--- tests/metrics_test.py
+++ tests/metrics_test.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from ir_eval.metrics import recall
+from ir_eval.metrics import recall, precision
 
 # Sample data generated with:
 # total_count_items = 100
@@ -16,10 +16,17 @@ predicted = np.array([1, 2, 62, 84, 3, 4, 81, 14, 5, 67])
 class TestRecall:
   def test_recall_k_5(self):
     result = recall(actual, predicted, 5)
-    np.testing.assert_allclose(result, 0.04)
+    assert result == pytest.approx(0.04)
   
   def test_recall_k_10(self):
     result = recall(actual, predicted, 10)
-    np.testing.assert_allclose(result, 0.12)
+    assert result == pytest.approx(0.12)
 
-
+class TestPrecision:
+  def test_precision_k_5(self):
+    result = precision(actual, predicted, 5)
+    assert result == pytest.approx(0.2)
+  
+  def test_precision_k_10(self):
+    result = precision(actual, predicted, 10)
+    assert result == pytest.approx(0.3)
