3fbf28dac9239c77408e79be43cdf550db5f5bca
Patrick Lurch <plurch@users.noreply.github.com>
2025-01-08 19:00:04 -0500

    set version number statically

--- src/ir_evaluation/__init__.py
+++ src/ir_evaluation/__init__.py
@@ -1,16 +1,5 @@
-import tomllib
-from pathlib import Path
+from . import metrics
 
-pyproject_path = Path(__file__).parent / '..' / '..' / "pyproject.toml"
+__version__ = "1.0.4"
 
-with open(pyproject_path, "rb") as f:
-  pyproject_data = tomllib.load(f)
-
-metadata = pyproject_data["project"]
-custom_metadata = pyproject_data["tool"]["ir_evaluation"]["metadata"]
-
-__version__ = metadata["version"]
-__description__ = metadata["description"]
-__author__ = metadata["authors"][0]["name"]
-__license__ = custom_metadata["license"]
-__url__ = custom_metadata["url"]
+__all__ = ["metrics", "__version__"]
